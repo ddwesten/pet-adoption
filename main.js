5377a037aeb93f65b19cbe72a148d9a2e2decaa0
dwesten <david@westensw.com>
2025-12-26 16:47:39 -0500

    added JavaScript to retrieve temperature in Miami

---
+++ main.js
@@ -0,0 +1,9 @@
+async function start()
+{
+    const weatherPromise=await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+    const weatherData = await weatherPromise.json()
+    const miamiTemp = weatherData.properties.periods[0].temperature
+    document.querySelector("#miami-temp").textContent = miamiTemp
+}
+
+start()
