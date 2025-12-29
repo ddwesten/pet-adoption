64ffa8337ca1cce8363279be6eb45b515fc9851b
dwesten <david@westensw.com>
2025-12-29 11:33:14 -0500

    create pet cards from database

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
 async function start()
 {
     const weatherPromise=await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
@@ -14,8 +17,24 @@ async function petsArea(params)
     const petsData = await petsPromise.json()
     petsData.forEach(pet => 
     {
-        console.log(pet.name)
+        const clone = template.content.cloneNode(true)
+        clone.querySelector("h3").textContent=pet.name
+        clone.querySelector(".pet-description").textContent = pet.description
+        clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+        clone.querySelector(".pet-card-photo img").src = pet.photo
+        clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`
+        wrapper.appendChild(clone)
     });
+    document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
 petsArea()
+
+function createAgeText(birthYear)
+{
+    const currentYear = 2022 //new Date().getFullYear()
+    const age = currentYear - birthYear
+    if (age == 0) return "Less than a year old"
+    if (age == 1) return "1 year old"
+    return age + " years old"
+}
